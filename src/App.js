d3002b1819c8e23876ba25de23e1addd0d71f0a7
luit <luit93@gmail.com>
2021-08-16 13:28:10 +1000

    implemented add task form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
+import { Container, Col, Row } from "react-bootstrap";
+import { AddTaskForm } from "./components/add-task-form/AddTaskForm";
 function App() {
-  return <div>3hh3h3</div>;
+  const [tasks, setTasks] = useState([]);
+  const handleOnSubmit = (data) => {
+    setTasks([...tasks, data]);
+  };
+  return (
+    <div>
+      <Container fluid>
+        <Row className="mt-5">
+          <Col>
+            <h1>Not To Do Task List</h1>
+          </Col>
+        </Row>
+        <hr />
+        <AddTaskForm handleSubmit={handleOnSubmit} />
+        <hr />
+      </Container>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/add-task-form/AddTaskForm.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { Col, Row, Button, Form } from "react-bootstrap";
+import React from "react";
+const initialFormData = {
+  task: "",
+  hr: 0,
+};
+export const AddTaskForm = ({ handleSubmit }) => {
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit(formData);
+  };
+  return (
+    <Form onSubmit={handleOnSubmit}>
+      <Row>
+        <Col xs={7}>
+          <Form.Control
+            name="task"
+            placeholder="Task"
+            required
+            onChange={handleOnChange}
+          />
+        </Col>
+        <Col>
+          <Form.Control
+            name="hour"
+            placeholder="Hours"
+            type="number"
+            required
+            onChange={handleOnChange}
+          />
+        </Col>
+        <Col>
+          <Button>Add Task</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
